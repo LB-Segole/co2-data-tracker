bf8253dd28daead7bbf92fb505fdea6e9a053e6d
LB-Segole <segolelesego17@gmail.com>
2026-09-10 16:55:31 +0200

    config apis

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // leave blank and it just runs off the demo listings below
 
 window.OFFTAKE_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://qgedhjkeykkwqbhjriqh.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_PnkL4KGTkYuBccUR7IzC7A_1E1HVA2r",
   TABLE_NAME: "co2_listings",
   CONTACT_INBOX: "sgldevelopments21@gmail.com"
 };
