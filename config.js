c71f80e7d60a67c0613f104dc72929571a9e393a
LB-Segole <segolelesego17@gmail.com>
2026-09-10 16:33:27 +0200

    fixed config wiring

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // fill these in once supabase project is set up, get them from project settings > api
-// leave blank and it just runs off the demo listings below
+// leave blank and it just runs off the demo listings
 
 window.OFFTAKE_CONFIG = {
   SUPABASE_URL: "https://qgedhjkeykkwqbhjriqh.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnZWRoamtleWtrd3FiaGpyaXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwMjk2MjQsImV4cCI6MjEwNDYwNTYyNH0.NsmhhOSc4GfrTVtSYRipQpK3Sjv72EztsIM0KD2e6xI",
+  SUPABASE_ANON_KEY: "sb_publishable_PnkL4KGTkYuBccUR7IzC7A_1E1HVA2r",
   TABLE_NAME: "co2_listings",
   CONTACT_INBOX: "sgldevelopment21@gmail.com"
 };
